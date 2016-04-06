d2778b7d2a1189c3f1202d1bb3425af19387057a
khalilgharbaoui <kaygeee@gmail.com>
2016-04-06 08:38:15 +0200

    added external mongoDB

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,10 +25,21 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    // webpack.config.dev.js
+    //voor style 🙄
+
+      loaders: [
+        {
+          test: /\.jsx?/,
+          loaders: ['babel'],
+          include: path.join(__dirname, 'src')
+        },
+        { test: /\.woff2?$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
+        { test: /\.ttf$/,    loader: "file-loader" },
+        { test: /\.eot$/,    loader: "file-loader" },
+        { test: /\.svg$/,    loader: "file-loader" },
+        { test: /\.png$/,    loader: "file-loader" },
+        { test: /\.scss$/,   loader: 'style!css!sass'}
+      ]
   }
 };
